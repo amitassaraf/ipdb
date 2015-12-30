0120567224f1ac7da54e64dc24e8114b4fc82e49
Amit Assaraf <amit@helpi.me>
2015-12-31 00:51:35 +0200

    Revert "Revert "Improved current ipdb to be better.""
    This reverts commit db90959eed6e1372321790dc8dabfb30183c7634.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2012 Marc Abramowitz
 #
-# This file is part of ipdb.
+# This file is part of pdbi.
 # GNU package is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 2 of the License, or (at your option)
@@ -17,7 +17,7 @@ import unittest
 class ImportTest(unittest.TestCase):
 
     def test_import(self):
-        from ipdb import set_trace, post_mortem, pm, run, runcall, runeval
+        from pdbi import set_trace, post_mortem, pm, run, runcall, runeval
         set_trace  # please pyflakes
         post_mortem  # please pyflakes
         pm  # please pyflakes
